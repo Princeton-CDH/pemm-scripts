f000e033bc8dd569a6ce432406153f23756d2000
Nick Budak <thatbudakguy@gmail.com>
2019-09-26 12:41:41 -0400

    add column start/end and page reproduction

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,25 @@ global.main = () => {
 
     /* create data validation */
     const manuscriptRule = SpreadsheetApp.newDataValidation()
-        .requireValueInRange(spreadsheet.getRangeByName('manuscripts__id'))
+        .requireValueInRange(spreadsheet.getRangeByName('manuscript__id'))
         .setHelpText('Manuscript ID must be listed on Manuscripts sheet.')
         .setAllowInvalid(false)
         .build()
 
     const repositoryRule = SpreadsheetApp.newDataValidation()
-        .requireValueInRange(spreadsheet.getRangeByName('repositories__name'))
+        .requireValueInRange(spreadsheet.getRangeByName('repository__name'))
         .setHelpText('Repository must be listed on Repositories sheet.')
         .setAllowInvalid(false)
         .build()
 
     const incipitRule = SpreadsheetApp.newDataValidation()
-        .requireValueInRange(spreadsheet.getRangeByName('canonical_stories__incipit'))
+        .requireValueInRange(spreadsheet.getRangeByName('canonical_story__incipit'))
         .setHelpText('Incipit must match a Canonical Story.')
         .setAllowInvalid(false)
         .build()
 
     const canonicalStoryRule = SpreadsheetApp.newDataValidation()
-        .requireValueInRange(spreadsheet.getRangeByName('canonical_stories__macomber_id'))
+        .requireValueInRange(spreadsheet.getRangeByName('canonical_story__macomber_id'))
         .setHelpText('Story instance must correspond to a Canonical Story.')
         .setAllowInvalid(false)
         .build()
@@ -38,6 +38,12 @@ global.main = () => {
     const folioRule = SpreadsheetApp.newDataValidation()
         .requireFormulaSatisfied('=REGEXMATCH(TO_TEXT(A2), "[1-9]\d*[rvab]?")')
         .setHelpText('Folio must be number optionally followed by [r,v,a,b].')
+        .setAllowInvalid(false)
+        .build()
+
+    const positiveIntegerRule = SpreadsheetApp.newDataValidation()
+        .requireNumberGreaterThan(0)
+        .setHelpText('Must be a positive integer.')
         .setAllowInvalid(false)
         .build()
 
@@ -58,6 +64,9 @@ global.main = () => {
     spreadsheet.getRangeByName('canonical_story__incipit')
         .setDataValidation(incipitRule)
 
+    spreadsheet.getRangeByName('canonical_story__macomber_id')
+        .setDataValidation(positiveIntegerRule)
+
     // story instance
     spreadsheet.getRangeByName('story_instance__manuscript')
         .setDataValidation(manuscriptRule)
@@ -67,4 +76,16 @@ global.main = () => {
 
     spreadsheet.getRangeByName('story_instance__folio')
         .setDataValidation(folioRule)
+
+    spreadsheet.getRangeByName('story_instance__column_start')
+        .setDataValidation(positiveIntegerRule)
+    
+    spreadsheet.getRangeByName('story_instance__line_start')
+        .setDataValidation(positiveIntegerRule)
+
+    spreadsheet.getRangeByName('story_instance__column_end')
+        .setDataValidation(positiveIntegerRule)
+
+    spreadsheet.getRangeByName('story_instance__line_end')
+        .setDataValidation(positiveIntegerRule)
 }
