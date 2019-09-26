7ddeac37370700e29ff9099fdd42d93d2962425e
Nick Budak <thatbudakguy@gmail.com>
2019-09-26 12:08:37 -0400

    add protected folio field

--- src/main.ts
+++ src/main.ts
@@ -2,52 +2,69 @@ import schema from './schema.json'
 import { setupSpreadsheet } from './lib'
 
 global.main = () => {
+    /* set up spreadsheet */
     const spreadsheet = setupSpreadsheet(schema)
 
-    // create data validation rules
+    /* create data validation */
     const manuscriptRule = SpreadsheetApp.newDataValidation()
         .requireValueInRange(spreadsheet.getRangeByName('manuscripts__id'))
-        .setAllowInvalid(false)
         .setHelpText('Manuscript ID must be listed on Manuscripts sheet.')
+        .setAllowInvalid(false)
         .build()
+
     const repositoryRule = SpreadsheetApp.newDataValidation()
         .requireValueInRange(spreadsheet.getRangeByName('repositories__name'))
-        .setAllowInvalid(false)
         .setHelpText('Repository must be listed on Repositories sheet.')
+        .setAllowInvalid(false)
         .build()
+
     const incipitRule = SpreadsheetApp.newDataValidation()
         .requireValueInRange(spreadsheet.getRangeByName('canonical_stories__incipit'))
-        .setAllowInvalid(false)
         .setHelpText('Incipit must match a Canonical Story.')
+        .setAllowInvalid(false)
         .build()
+
     const canonicalStoryRule = SpreadsheetApp.newDataValidation()
         .requireValueInRange(spreadsheet.getRangeByName('canonical_stories__macomber_id'))
-        .setAllowInvalid(false)
         .setHelpText('Story instance must correspond to a Canonical Story.')
+        .setAllowInvalid(false)
         .build()
+
     const booleanRule = SpreadsheetApp.newDataValidation()
         .requireCheckbox()
         .setAllowInvalid(false)
         .build()
+
+    const folioRule = SpreadsheetApp.newDataValidation()
+        .requireFormulaSatisfied('=REGEXMATCH(TO_TEXT(A2), "[1-9]\d*[rvab]?")')
+        .setHelpText('Folio must be number optionally followed by [r,v,a,b].')
+        .setAllowInvalid(false)
+        .build()
+
+    /* apply rules/formulas */
     
-    // apply data validation rules and formulas
-    spreadsheet.getSheetByName('Manuscripts')
-        .getRange('C2:C')
+    // manuscript
+    spreadsheet.getRangeByName('manuscript__repository')
         .setDataValidation(repositoryRule)
-    spreadsheet.getSheetByName('Manuscripts')
-        .getRange('D2:D')
+
+    spreadsheet.getRangeByName('manuscript__original_repository')
         .setDataValidation(repositoryRule)
-    spreadsheet.getSheetByName('Canonical Stories')
-        .getRange('E2:E')
+
+    // canonical story
+    spreadsheet.getRangeByName('canonical_story__noneuropean_origin')
         .setDataValidation(booleanRule)
         .setFormula('=if(and(not(isblank(A2)), not(isblank(C2)), isblank(D2)), true,)')
-    spreadsheet.getSheetByName('Canonical Stories')
-        .getRange('F2:F')
+
+    spreadsheet.getRangeByName('canonical_story__incipit')
         .setDataValidation(incipitRule)
-    spreadsheet.getSheetByName('Story Instances')
-        .getRange('A2:A')
+
+    // story instance
+    spreadsheet.getRangeByName('story_instance__manuscript')
         .setDataValidation(manuscriptRule)
-    spreadsheet.getSheetByName('Story Instances')
-        .getRange('B2:B')
+
+    spreadsheet.getRangeByName('story_instance__canonical_story')
         .setDataValidation(canonicalStoryRule)
+
+    spreadsheet.getRangeByName('story_instance__folio')
+        .setDataValidation(folioRule)
 }
