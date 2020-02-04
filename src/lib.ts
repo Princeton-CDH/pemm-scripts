50719a3fef474c8f5824ad475793a1e9cd4e1cc0
Nick Budak <nbudak@princeton.edu>
2020-02-04 16:31:14 -0500

    remove interfaces from lib, add array.find polyfill

--- src/lib.ts
+++ src/lib.ts
@@ -1,24 +1,5 @@
-// convenience type aliases for imported types
-type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet
-type Sheet = GoogleAppsScript.Spreadsheet.Sheet
-
-// configuration object type for creating a Spreadsheet
-interface SpreadsheetSchema {
-    title: string,
-    sheets: SheetSchema[],
-}
-
-// configuration object type for creating a Sheet
-interface SheetSchema {
-    name: string,
-    fields: FieldSchema[],
-}
-
-// configuration object type for creating a Field
-interface FieldSchema {
-    name: string,
-    protected?: boolean
-}
+import { Sheet, SheetSchema } from './sheet'
+import { Spreadsheet, SpreadsheetSchema } from './spreadsheet'
 
 // pre-built styles
 export const headerStyle = SpreadsheetApp.newTextStyle().setBold(true).build()
@@ -120,3 +101,52 @@ export const indexToAlpha = (index: number): string => {
         .map(digit => String.fromCharCode(65 + digit)) // use the digit as an offset from 'A' (ASCII 65)
         .join('') // join resulting letters back together
 }
+
+// array.find polyfill, see:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find#Polyfill
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+      value: function(predicate: any) {
+        // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw TypeError('"this" is null or not defined');
+        }
+  
+        var o = Object(this);
+  
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+  
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw TypeError('predicate must be a function');
+        }
+  
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+  
+        // 5. Let k be 0.
+        var k = 0;
+  
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return kValue.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return kValue;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+  
+        // 7. Return undefined.
+        return undefined;
+      },
+      configurable: true,
+      writable: true
+    });
+  }
