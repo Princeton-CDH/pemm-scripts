49fb7e155b9a78e1f8c1b6aeda23dd98ffda4a39
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-02-04 18:19:50 -0500

    Test get incipit

--- tests/test_macomber_to_csv.py
+++ tests/test_macomber_to_csv.py
@@ -19,11 +19,19 @@ class TestMacomberToCSV:
         assert self.mac.schema['sheets']
         assert self.mac.schema['sheets'][0]['name'] == 'Story Instance'
 
+    incipit_start = 'በእንተ፡ ዘከመ፡ አስተርአየቶ፡ ለቴዎፍሎስ፡ ሊቀ፡'
+
     def test_load_incipits(self):
         self.mac.load_incipits(os.path.join(self.data_dir, 'incipits.csv'))
         assert self.mac.incipits
         assert '1-A' in self.mac.incipits
         assert 'EMML' in self.mac.incipits['1-A']
         assert '6938' in self.mac.incipits['1-A']['EMML']
-        incipit_start = 'በእንተ፡ ዘከመ፡ አስተርአየቶ፡ ለቴዎፍሎስ፡ ሊቀ፡'
-        assert self.mac.incipits['1-A']['EMML']['6938'].startswith(incipit_start)
+        assert self.mac.incipits['1-A']['EMML']['6938'] \
+            .startswith(self.incipit_start)
+
+    def test_get_incipit(self):
+        incipit = self.mac.get_incipit('1-A', 'EMML', '6938')
+        assert incipit.startswith(self.incipit_start)
+        # missing
+        assert self.mac.get_incipit('1-A', 'EMML', 'foo') == ''
