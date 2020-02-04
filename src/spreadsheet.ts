2a6413ede91dac82d8448974b6b229aa14429b3f
Nick Budak <nbudak@princeton.edu>
2020-02-04 16:54:27 -0500

    move setupSpreadsheet to spreadsheet module

--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -1,4 +1,5 @@
-import { SheetSchema } from './sheet'
+import { SheetSchema, setupSheet } from './sheet'
+import { setupValidation } from './validation'
 
 // convenience type alias
 export type Spreadsheet = GoogleAppsScript.Spreadsheet.Spreadsheet
@@ -9,5 +10,29 @@ export interface SpreadsheetSchema {
     sheets: SheetSchema[],
 }
 
+/**
+ * Sets up all sheets on a given spreadsheet according to a provided schema,
+ * creating each sheet if it doesn't already exist.
+ * 
+ * Applies all data validation.
+ * 
+ * @param spreadsheet
+ * @param schema 
+ */
+export const setupSpreadsheet = (spreadsheet: Spreadsheet, schema: SpreadsheetSchema): Spreadsheet => {
+    schema.sheets.forEach(({ name, fields }) => {
+        // if the sheet doesn't exist yet, create it
+        let sheet = spreadsheet.getSheetByName(name)
+        if (sheet === null) sheet = spreadsheet.insertSheet(name)
+        
+        // call setupSheet to set up the sheet
+        setupSheet(spreadsheet, sheet, { name, fields })
+    })
 
+    // apply data validation
+    setupValidation(spreadsheet)
+    
+    // return spreadsheet when setup completed
+    return spreadsheet
+}
 
