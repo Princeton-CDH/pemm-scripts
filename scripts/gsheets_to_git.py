d153d645d877c9b4546d688642b0eed912a65f29
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-03-16 16:01:50 -0400

    Refactor to a class to share properties across methods

--- scripts/gsheets_to_git.py
+++ scripts/gsheets_to_git.py
@@ -43,9 +43,10 @@ from datetime import datetime
 from types import SimpleNamespace
 
 from git import InvalidGitRepositoryError, Repo
+from googleapiclient.discovery import build
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import requests
 
 
 def sheet_filename(worksheet, mode='csv'):
@@ -58,159 +59,178 @@ def pad_csv_row(values, size):
     return (values + [''] * (size - len(values)))
 
 
-def init_gsheets_client():
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
+class GSheetsToGit:
 
-    # NOTE: currently assumes credentials are in the directory
-    # where the script is being run
-    credentials = ServiceAccountCredentials \
-        .from_json_keyfile_name('pemm_credentials.json', scope)
+    # default data dir within the git repo
+    DEFAULT_DATA_DIR = 'data'
 
-    return gspread.authorize(credentials)
+    # ISO format used for dates
+    MODIFIED_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
-# ISO format used for dates
-MODIFIED_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+    # filename for last run information (stored in current user's home)
+    lastrun_filename = os.path.join(os.path.expanduser('~'), '.gsheets_to_git')
 
+    # list of files generated from spreadsheet data
+    updated_filenames = []
+    # name of the spreadsheet
+    gsheet_title = None
 
-def get_document_lastmodified(session, docid, page_token=None):
-    # NOTE: we could use google api python client, but it's pretty clunky;
-    # instead, using the session with credentials configured by gspread
+    def __init__(self, docid, gitpath, datadir=None):
+        self.docid = docid
+        self.gitpath = gitpath
+        self.datadir = datadir or self.DEFAULT_DATA_DIR
+        self.outdir = os.path.join(args.gitpath, args.datadir)
 
-    # GET https://www.googleapis.com/drive/v3/files/fileId
-    get_file_url = 'https://www.googleapis.com/drive/v3/files/%s' % docid
-    opts = {
-        'fields': 'modifiedTime',
-        # will be deprecated in June 2020, but 404s without
-        'supportsAllDrives': 'true'
-    }
-    response = session.get(get_file_url, params=opts)
-    if response.status_code == requests.codes.ok:
-        # convert to datetime
-        # (unfortunately not on py3.7 so can't use from isoformat)
-        return datetime.strptime(response.json()['modifiedTime'],
-                                 MODIFIED_TIME_FORMAT)
+        self.init_google_clients()
 
+        # synchronize to local csv
+        self.gsheet_to_csv()
+        # if there were changes, commit to git
+        if self.updated_filenames:
+            self.update_gitrepo()
 
-# *** methods for handling information about script last run **
+    def init_google_clients(self):
+        '''initialize google drive api and gspread client'''
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
 
+        # NOTE: currently assumes credentials are in the directory
+        # where the script is being run
+        credentials = ServiceAccountCredentials \
+            .from_json_keyfile_name('pemm_credentials.json', scope)
 
-LASTRUN_FILENAME = '.gsheets_to_git'
+        # initialize gspread client
+        self.gspread = gspread.authorize(credentials)
+        # initialize google drive api client
+        self.drive_api = build('drive', 'v3', credentials=credentials)
 
+    def get_lastrun_info(self):
+        # check for information about the last run of this script
+        # load as json if it is exists, returns None otherwise
+        if os.path.exists(self.lastrun_filename):
+            with open(self.lastrun_filename) as lastrun:
+                return json.load(lastrun)
 
-def lastrun_filename():
-    # store script last run information in user's home directory
-    return os.path.join(os.path.expanduser('~'), LASTRUN_FILENAME)
+    def update_lastrun_info(self, updates):
+        # Update or create last run information file
+        lastrun_info = self.get_lastrun_info() or {}
+        lastrun_info.update(updates)
+        with open(self.lastrun_filename, 'w') as lastrun:
+                return json.dump(lastrun_info, lastrun, indent=2)
 
+    def parse_time(self, timestr):
+        # convert timestamp string to datetime object
+        # (unfortunately can't use py3.7 fromisoformat)
+        return datetime.strptime(timestr, self.MODIFIED_TIME_FORMAT)
 
-def get_lastrun_info():
-    # check for information about the last run of this script for this gsheet
-    lastrun_file = lastrun_filename()
-    if os.path.exists(lastrun_file):
-        with open(lastrun_file) as lastrun:
-            return json.load(lastrun)
+    def format_time(self, datetimeobj):
+        return datetimeobj.strftime(self.MODIFIED_TIME_FORMAT)
 
+    def get_document_lastmodified(self):
+        results = self.drive_api.files() \
+            .get(fileId=self.docid,
+                 supportsAllDrives=True, fields='modifiedTime').execute()
+        return self.parse_time(results.get('modifiedTime'))
 
-def update_lastrun_info(updates):
-    # Update or create last run information file
-    lastrun_file = lastrun_filename()
-    lastrun_info = get_lastrun_info() or {}
-    lastrun_info.update(updates)
-    with open(lastrun_file, 'w') as lastrun:
-            return json.dump(lastrun_info, lastrun, indent=2)
+    # simple property caching
+    _script_lastrun = None
 
+    @property
+    def script_lastrun(self):
+        # determine the last modification timestamp for the last run
+        # of this script on this document
 
-def gsheet_to_csv(docid, outdir):
-    '''Use Google Sheets API to grab google sheets content for the
-    given document and save all sheets as CSV.
+        if self._script_lastrun is None:
+            # load information about the last run of this script
+            lastrun_data = self.get_lastrun_info()
+            # if the file exists, pull out modified value for this docid
+            if lastrun_data and self.docid in lastrun_data['modified']:
+                self._script_lastrun = self.parse_time(
+                    lastrun_data['modified'][self.docid])
 
-    Returns a list of the files created and the name of the document.
-    '''
-    gsheets_client = init_gsheets_client()
-    # determine when the document was last modified
-    gsheet_lastmod = get_document_lastmodified(gsheets_client.session, docid)
-    # check for information about the last run of this script for this gsheet
-    lastrun_data = get_lastrun_info()
+        return self._script_lastrun
 
-    # if we have a last modified date, compare against document
-    if lastrun_data and docid in lastrun_data['modified']:
-        script_lastrun = datetime.strptime(lastrun_data['modified'][docid],
-                                           MODIFIED_TIME_FORMAT)
-        # if not modified, bail out
-        if gsheet_lastmod == script_lastrun:
+    def gsheet_to_csv(self):
+        '''Use Google Sheets API to grab google sheets content for the
+        given document and save all sheets as CSV.  If document has not
+        been modified since the last script run, does nothing.
+
+        Stores a list of the files created and document title on the class.
+        '''
+        # determine when the document was last modified
+        gsheet_lastmod = self.get_document_lastmodified()
+
+        # if we have a last script run modified date, compare against document
+        if self.script_lastrun and gsheet_lastmod == self.script_lastrun:
+            # if not modified, bail out
             print('No changes since last run')
             return
 
-    gsheet = gsheets_client.open_by_key(docid)
+        gsheet = self.gspread.open_by_key(self.docid)
+        self.gsheet_title = gsheet.title
 
-    # make sure the output dir exists
-    if not os.path.isdir(outdir):
-        os.mkdir(outdir)
+        # make sure the output dir exists
+        if not os.path.isdir(self.outdir):
+            os.mkdir(self.outdir)
 
-    # get all worksheets
-    worksheets = gsheet.worksheets()
-    filenames = []
-    for sheet in worksheets:
-        # do NOT synchronize github contributor emails
-        if sheet.title == '_contributors':
-            continue
-        filename = os.path.join(outdir, sheet_filename(sheet))
-        filenames.append(filename)
-        with open(filename, 'w') as csvfile:
-            print('Saving %s as %s' % (sheet.title, sheet_filename(sheet)))
-            csvwriter = csv.writer(csvfile)
-            sheet_data = sheet.get_all_values()
-            # determine the length of this sheet
-            columns = len(sheet_data[0])
-            # if rows are equal length, github will display nicely
-            csvwriter.writerows([pad_csv_row(row, columns)
-                                 for row in sheet_data])
+        # get all worksheets
+        worksheets = gsheet.worksheets()
+        for sheet in worksheets:
+            # do NOT synchronize github contributor emails
+            if sheet.title == '_contributors':
+                continue
+            filename = os.path.join(self.outdir, sheet_filename(sheet))
+            self.updated_filenames.append(filename)
+            with open(filename, 'w') as csvfile:
+                print('Saving %s as %s' % (sheet.title, sheet_filename(sheet)))
+                csvwriter = csv.writer(csvfile)
+                sheet_data = sheet.get_all_values()
+                # determine the length of this sheet
+                columns = len(sheet_data[0])
+                # if rows are equal length, github will display nicely
+                csvwriter.writerows([pad_csv_row(row, columns)
+                                     for row in sheet_data])
 
-    # update the lastrun file with document last modification time
-    update_lastrun_info({'modified': {
-        docid: gsheet_lastmod.strftime(MODIFIED_TIME_FORMAT)
-    }})
+        # update the lastrun file with document last modification time
+        self.update_lastrun_info({
+            'modified': {
+                self.docid: self.format_time(gsheet_lastmod)
+            }})
 
-    return (filenames, gsheet.title)
-
-
-def update_gitrepo(repo_path, files, doctitle):
-    '''Update the git repository and push changes.'''
-    try:
-        repo = Repo(repo_path)
-    except InvalidGitRepositoryError:
-        print('%s is not a valid git repository' % repo_path)
-        return
-
-    repo.index.add(files)
-    if repo.is_dirty():
-        print('Committing changes')
-        repo.index.commit('Automatic data updates from %s' % doctitle)
-
+    def update_gitrepo(self):
+        '''Update the git repository and push changes.'''
         try:
-            origin = repo.remote(name='origin')
-            # pull any remote changes
-            origin.pull()
-            # push data updates
-            result = origin.push()
-            # output push summary in case anything bad happens
-            for pushinfo in result:
-                print(pushinfo.summary)
-        except ValueError:
-            print('No origin repository, unable to push updates')
-    else:
-        print('No changes')
+            repo = Repo(self.gitpath)
+        except InvalidGitRepositoryError:
+            print('%s is not a valid git repository' % self.gitpath)
+            return
 
+        repo.index.add(self.updated_filenames)
+        if repo.is_dirty():
+            print('Committing changes')
+            repo.index.commit('Automatic data updates from %s' %
+                              self.gsheet_title)
 
-# default data dir within the git repo
-DEFAULT_DATA_DIR = 'data'
+            try:
+                origin = repo.remote(name='origin')
+                # pull any remote changes
+                origin.pull()
+                # push data updates
+                result = origin.push()
+                # output push summary in case anything bad happens
+                for pushinfo in result:
+                    print(pushinfo.summary)
+            except ValueError:
+                print('No origin repository, unable to push updates')
+        else:
+            print('No changes')
 
 
 def get_env_opts():
     # check for environment variable configuration
     return SimpleNamespace(
         gitpath=os.getenv('PEMM_DATA_REPO_PATH', None),
-        datadir=os.getenv('PEMM_DATA_REPO_DATADIR', DEFAULT_DATA_DIR),
+        datadir=os.getenv('PEMM_DATA_REPO_DATADIR', None),
         docid=os.getenv('PEMM_GSHEETS_DOCID', None)
     )
 
@@ -220,7 +240,7 @@ def get_cli_args():
     parser = argparse.ArgumentParser(
         description='''Retrieve Google Sheets data as CSV and add to or
     update in a git repository.''')
-    parser.add_argument('-d', '--datadir', default=DEFAULT_DATA_DIR,
+    parser.add_argument('-d', '--datadir', default=GSheetsToGit.DEFAULT_DATA_DIR,
                         help='Data directory within the git repository')
     parser.add_argument('-g', '--gitpath', required=True,
                         help='Git repository working directory')
@@ -236,9 +256,7 @@ if __name__ == "__main__":
     if not args.gitpath or not args.docid:
         args = get_cli_args()
 
-    result = gsheet_to_csv(
-        args.docid, os.path.join(args.gitpath, args.datadir))
-    # if something was returned, there are changes to commit
-    if result:
-        filenames, doctitle = result
-        update_gitrepo(args.gitpath, filenames, doctitle)
+    # NOTE: arg parsing could probably be better, and could be handled
+    # within the class, but leaving as is for now
+
+    GSheetsToGit(args.docid, args.gitpath, args.datadir)
